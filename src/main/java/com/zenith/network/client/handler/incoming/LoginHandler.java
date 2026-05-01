14b77f60d2349a5ce0156a5531f22417b0fc7a40
aquariusnetwork9 <aquariusmediarequests@gmail.com>
2026-05-01 10:27:41 -0500

    Update LoginHandler.java

--- src/main/java/com/zenith/network/client/handler/incoming/LoginHandler.java
+++ src/main/java/com/zenith/network/client/handler/incoming/LoginHandler.java
@@ -4,6 +4,7 @@ import com.zenith.Proxy;
 import com.zenith.cache.CacheResetType;
 import com.zenith.event.client.ClientOnlineEvent;
 import com.zenith.network.client.ClientSession;
+import com.zenith.module.impl.ServerLogin;
 import com.zenith.network.codec.PacketHandler;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.clientbound.ClientboundLoginPacket;
 import org.geysermc.mcprotocollib.protocol.packet.ingame.serverbound.ServerboundChatSessionUpdatePacket;
@@ -73,7 +74,7 @@ public class LoginHandler implements PacketHandler<ClientboundLoginPacket, Clien
         }
 
         if (!Proxy.getInstance().isOn2b2t()) {
-            if (!CONFIG.authentication.serverLoginRequired) {
+            if (!CONFIG.authentication.serverLoginRequired || ServerLogin.isAuthComplete()) {
                 if (!session.isOnline()) {
                     session.setOnline(true);
                     EVENT_BUS.post(new ClientOnlineEvent());
